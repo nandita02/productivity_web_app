a27937bbd33eaa66ae47062181de8b58d773bfca
nandita mathur <61509005+nandita02@users.noreply.github.com>
2022-01-19 16:18:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// // $( "i" ).hover(function() {
+// //     $( this ).fadeOut( 100 );
+// //     $( this ).fadeIn( 500 );
+// //   });
+
+// $(".fas").hover(function(){
+//     $("i").addClass("pressed");
+//     setTimeout(function(){
+//         $("i").removeClass("pressed");
+//     },200);
+// })
+
---
+++ todo/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCEkqDxIl583DOefLP-wfVfXirTEu-heF8",
+  authDomain: "todo-f3000.firebaseapp.com",
+  projectId: "todo-f3000",
+  storageBucket: "todo-f3000.appspot.com",
+  messagingSenderId: "286884303650",
+  appId: "1:286884303650:web:4e84a2eaf8894d27098b7b"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
